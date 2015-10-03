628ff70e7ff4d00adb9cd478b88a1af16f62491c
peter <peter.k.nets@gmail.com>
2015-10-03 20:59:29 +0300

    Add bootsrtap and other libs

--- app/app.js
+++ app/app.js
@@ -10,3 +10,7 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+function StoreController() {
+    alert("Welcome!");
+}
